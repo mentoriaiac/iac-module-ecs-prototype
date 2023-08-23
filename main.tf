f3dcd059a1a30d50f42071d5abf8f521ccf9a161
silmarasilva <silmara.eliza@gmail.com>
2023-08-22 21:23:10 -0300

    feat: adding resources day 3

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.12.0"
     }
   }
@@ -15,26 +15,48 @@ resource "aws_ecs_cluster" "cluster" {
   name = "mentoria-teste"
 }
 
+resource "aws_ecs_cluster_capacity_providers" "example" {
+  cluster_name = aws_ecs_cluster.cluster.name
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  }
+}
+
+resource "aws_route" "internet_gateway" {
+  route_table_id         = aws_vpc.main.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
 
 resource "aws_security_group" "ecs" {
-  name        = "ecs-teste"
-  vpc_id      = aws_vpc.main.id
+  name   = "ecs-teste"
+  vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-    ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -47,12 +69,12 @@ resource "aws_security_group" "ecs" {
 }
 
 resource "aws_ecs_task_definition" "service" {
-  family = "sample-fargate"
-  memory = 512
-  cpu = 256
+  family                   = "sample-fargate"
+  memory                   = 512
+  cpu                      = 256
   requires_compatibilities = ["FARGATE"]
-  network_mode = "awsvpc"
-  container_definitions = <<EOF
+  network_mode             = "awsvpc"
+  container_definitions    = <<EOF
  [
         {
             "name": "fargate-app", 
@@ -79,10 +101,10 @@ resource "aws_ecs_task_definition" "service" {
 
 resource "aws_ecs_service" "apache" {
   name            = "apache"
-  cluster         = aws_ecs_cluster.cluster.id
+  cluster         = aws_ecs_cluster.test.id
   task_definition = aws_ecs_task_definition.service.arn
   desired_count   = 1
-  launch_type = "FARGATE"
+  launch_type     = "FARGATE"
   network_configuration {
     subnets = [aws_subnet.ecs.id]
   }
@@ -93,3 +115,4 @@ resource "aws_subnet" "ecs" {
   cidr_block = "10.0.1.0/24"
 
 }
+
