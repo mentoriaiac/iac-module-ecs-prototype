69b7e3420266ae41e8f6c2f7b6efb7fd0344deaa
William Dias <will.kof1@gmail.com>
2023-10-11 20:02:10 -0300

    adicionou config de security group no modulo de redes

--- main.tf
+++ main.tf
@@ -18,6 +18,26 @@ module "rede_prototipo" {
     primaria   = "10.1.1.0/24",
     secundaria = "10.1.2.0/24",
   }
+  ingress_rules = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+  ]
 }
 
 resource "aws_ecs_cluster" "cluster" {
--- modules/rede/main.tf
+++ modules/rede/main.tf
@@ -22,18 +22,14 @@ resource "aws_security_group" "ecs" {
   name   = "ecs-teste"
   vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
 
   egress {
