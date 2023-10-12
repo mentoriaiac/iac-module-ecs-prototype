48fb6ebf6b930d87c2eccbe010defe6c1014e837
gianlcl <giansama0@gmail.com>
2023-10-11 21:31:19 -0300

    alterado para usar modulos novos criados

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ terraform {
 }
 
 provider "aws" {
+  region = "us-east-1"
   # Configuration options
 }
 
@@ -40,106 +41,86 @@ module "rede_prototipo" {
   ]
 }
 
-resource "aws_ecs_cluster" "cluster" {
+module "cluster" {
+  source = "./modules/ecs-cluster"
+
   name = "mentoria-teste"
 }
 
-resource "aws_ecs_cluster_capacity_providers" "example" {
-  cluster_name = aws_ecs_cluster.cluster.name
+module "apache" {
+  source = "./modules/ecs-app"
 
-  capacity_providers = ["FARGATE"]
-
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE"
-  }
-}
-resource "aws_ecs_task_definition" "service" {
-  family                   = "sample-fargate"
-  memory                   = 512
-  cpu                      = 256
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  container_definitions    = <<EOF
- [
-        {
-            "name": "fargate-app", 
-            "image": "public.ecr.aws/docker/library/httpd:latest", 
-            "portMappings": [
-                {
-                    "containerPort": 80, 
-                    "hostPort": 80, 
-                    "protocol": "tcp"
-                }
-            ], 
-            "essential": true, 
-            "entryPoint": [
-                "sh",
-        "-c"
-            ], 
-            "command": [
-                "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
-            ]
-        }
-    ]
-  EOF
-}
-
-resource "aws_ecs_service" "apache" {
   name            = "apache"
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.service.arn
+  cluster_id      = module.cluster.cluster_id
   desired_count   = 1
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = [module.rede_prototipo.subnet_id.primaria]
-    assign_public_ip = true
-    security_groups  = [module.rede_prototipo.security_group_id]
+  subnets         = [module.rede_prototipo.subnet_id.primaria]
+  security_groups = [module.rede_prototipo.security_group_id]
+  resources = {
+    cpu    = 256
+    memory = 512
   }
-}
 
-
-resource "aws_ecs_task_definition" "service2" {
-  family                   = "sample-fargate"
-  memory                   = 512
-  cpu                      = 256
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  container_definitions    = <<EOF
- [
-        {
-            "name": "fargate-app", 
-            "image": "public.ecr.aws/docker/library/httpd:latest", 
-            "portMappings": [
-                {
-                    "containerPort": 80, 
-                    "hostPort": 80, 
-                    "protocol": "tcp"
-                }
-            ], 
-            "essential": true, 
-            "entryPoint": [
-                "sh",
-        "-c"
-            ], 
-            "command": [
-                "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
-            ]
-        }
+  container_definitions = <<EOF
+    [
+      {
+        "name": "fargate-app", 
+        "image": "public.ecr.aws/docker/library/httpd:latest", 
+        "portMappings": [
+          {
+            "containerPort": 80, 
+            "hostPort": 80, 
+            "protocol": "tcp"
+          }
+        ], 
+        "essential": true, 
+        "entryPoint": [
+          "sh",
+          "-c"
+        ], 
+        "command": [
+          "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+        ]
+      }
     ]
-  EOF
+    EOF
+
 }
 
-resource "aws_ecs_service" "apache2" {
+module "apache2" {
+  source = "./modules/ecs-app"
+
   name            = "apache2"
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.service2.arn
+  cluster_id      = module.cluster.cluster_id
   desired_count   = 1
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = [module.rede_prototipo.subnet_id.secundaria]
-    assign_public_ip = true
-    security_groups  = [module.rede_prototipo.security_group_id]
+  subnets         = [module.rede_prototipo.subnet_id.secundaria]
+  security_groups = [module.rede_prototipo.security_group_id]
+  resources = {
+    cpu    = 256
+    memory = 512
   }
+
+  container_definitions = <<EOF
+    [
+      {
+        "name": "fargate-app", 
+        "image": "public.ecr.aws/docker/library/httpd:latest", 
+        "portMappings": [
+          {
+            "containerPort": 80, 
+            "hostPort": 80, 
+            "protocol": "tcp"
+          }
+        ], 
+        "essential": true, 
+        "entryPoint": [
+          "sh",
+          "-c"
+        ], 
+        "command": [
+          "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+        ]
+      }
+    ]
+    EOF
+
 }
